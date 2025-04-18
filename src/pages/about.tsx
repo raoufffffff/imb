e76bb1920396e8e8fc666ee719276201d1b3122b
raoufffffff <112146388+raoufffffff@users.noreply.github.com>
2025-04-18 04:53:18 +0100

    Add files via upload

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,4 @@
-import type { Route } from "./+types/home";
-export function meta({ }: Route.MetaArgs) {
-    return [
-        { title: "about us" },
-        { name: "description", content: "Welcome to React Router!" },
-    ];
-}
+
 const About = () => {
     return (
         <div>About</div>
